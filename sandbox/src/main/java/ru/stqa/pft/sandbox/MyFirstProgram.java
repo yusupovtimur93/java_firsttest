265b4255f5c211a58ad94a1fbf838b831c1b051c
yusupovtimur93 <59929600+yusupovtimur93@users.noreply.github.com>
2020-01-20 22:38:45 +0500

    реализован конструктор внутри класса

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,12 +12,9 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(4,6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и "+ r.b + " = " + r.area());
-		Point p = new Point();
-		p.p1 = 5;
-		p.p2 = 50;
-		p.p3 = 50;
-		p.p4 = 50;
-		System.out.println("Расстояния между двумя точками =" + distance(p));
+
+		Point p = new Point(5, 50, 50, 50);
+		System.out.println("Расстояния между двумя точками = " + distance(p));
 	}
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,4 +5,12 @@ public class Point {
     public double p2;
     public double p3;
     public double p4;
+
+    public Point(double p1, double p2, double p3, double p4) {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+        this.p4 = p4;
+
+    }
 }
