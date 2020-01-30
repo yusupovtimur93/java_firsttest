492c78a0137afa1b0e4419d3acd4871f5e49a66c
yusupovtimur93 <59929600+yusupovtimur93@users.noreply.github.com>
2020-01-30 21:55:25 +0500

    в setup вынес логин в систему

--- addressbook/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -23,11 +23,6 @@ public class GroupCreationTests {
         driver = new FirefoxDriver();
         baseUrl = "https://www.google.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
-
-
-    @Test
-    public void testGroupCreationTests() throws Exception {
         driver.get("http://localhost/addressbook/");
         driver.findElement(By.name("user")).click();
         driver.findElement(By.name("user")).clear();
@@ -35,6 +30,12 @@ public class GroupCreationTests {
         driver.findElement(By.name("pass")).clear();
         driver.findElement(By.name("pass")).sendKeys("secret");
         driver.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+
+    @Test
+    public void testGroupCreation() throws Exception {
+
         driver.findElement(By.linkText("groups")).click();
         driver.findElement(By.name("new")).click();
         driver.findElement(By.name("group_name")).click();
