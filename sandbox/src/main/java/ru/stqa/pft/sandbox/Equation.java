53626ff04e08daf7412811ae362d1630347a7a96
yusupovtimur93 <59929600+yusupovtimur93@users.noreply.github.com>
2020-02-11 23:01:18 +0500

    добавление проверки линейного уровн

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -12,15 +12,33 @@ public class Equation {
         this.c = c;
 
         double d = b*b - 4*a*c;
-        if (d>0) {
-            n = 2;
-        } else {
-            if (d == 0) {
-                n = 1;
+        if (a==0) {
+            if (b==0) {
+                if (c==0) {
+                    n = -1;
+                } else {
+                    n = 0;
+                }
             } else {
-                n = 0;
+                n = 1;
+            }
+
+        } else {
+            if (d>0) {
+                n = 2;
+            } else {
+                if (d == 0) {
+                    n = 1;
+                } else {
+                    n = 0;
+                }
             }
         }
+
+
+
+
+
     }
     public int rootNumber(){
         return n;
