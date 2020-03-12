324c1c8caa913c965f5608c7776ee3873841dbfd
yusupovtimur93 <59929600+yusupovtimur93@users.noreply.github.com>
2020-03-12 14:57:17 +0500

    цикл и тесты Prime

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+    @Test
+    public void testPrime() {
+        Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+    }
+    @Test
+    public void testNonPrime() {
+        Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+    }
+}
