d40bfe39f6203b37405087d5cf9d7b0971500ae6
yusupovtimur93 <59929600+yusupovtimur93@users.noreply.github.com>
2020-03-12 22:25:32 +0500

    множества неупорядоченные коллекции

--- addressbook/src/test/java/ru/stqa/pft/addressbook/model/GroupDate.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/model/GroupDate.java
@@ -1,14 +1,28 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public class GroupDate {
     private final String name;
+    private final String id;
     private final String header;
     private final String footer;
 
-    public GroupDate(String name, String header, String footer) {
+    public GroupDate(String id,String name, String header, String footer) {
+        this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
+    }
+    public GroupDate(String name, String header, String footer) {
+        this.id = null;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getName() {
@@ -21,5 +35,27 @@ public class GroupDate {
 
     public String getFooter() {
         return footer;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupDate{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object text) {
+        if (this == text) return true;
+        if (text == null || getClass() != text.getClass()) return false;
+        GroupDate groupDate = (GroupDate) text;
+        return Objects.equals(name, groupDate.name) &&
+                Objects.equals(id, groupDate.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
     }
 }
--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase{
@@ -18,11 +19,15 @@ public class GroupModificationTests extends TestBase{
         List<GroupDate> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupDate("test1", null, null));
+        GroupDate group = new GroupDate(before.get(before.size() - 1).getId(),"test1", null, null);
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().sumbitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupDate> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
-    }
 
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    }
 }
